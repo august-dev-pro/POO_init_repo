e3fe9bf0da32b32bad7d6a2db08d94090dc2b93a
august-dev-pro <143771382+august-dev-pro@users.noreply.github.com>
2023-09-17 13:32:31 +0000

    creation d'une instance avec la methode constructeur

--- POO_exercice3.php
+++ POO_exercice3.php
@@ -1,7 +1,12 @@
 <?php
-    class personneS{
+    class Personne{
+
+        // ajout d'un constructeur
         public function construct_instance($nom, $age){
             $this->nom = $non;
             $this->age = $age;
         }
     }
+//creeation d'instance avec la methode constructeur
+
+$instancePersonne = new Personne("AUGUSTIN", 15);
