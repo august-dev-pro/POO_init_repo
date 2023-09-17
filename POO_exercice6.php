d40c599cc347bf02ac1a024b904f39ac7107594a
august-dev-pro <augustinselete01@gmail.com>
2023-09-17 12:38:56 +0000

    ajout des class heritieres carre et cercle et ajout des instance et appel de la mhethode calculerSurface dans les class heritiere

--- POO_exercice6.php
+++ POO_exercice6.php
@@ -2,17 +2,18 @@
     class MaClass{
         public $longeur;
         public $largeur;
-        
+
+    //methode constructeur       
     public function __construct($longueur, $largeur) {
         $this->longueur = $longueur;
         $this->largeur = $largeur;
     }
-
+    //methode de calcule surface
     public function calculerSurface() {
         return $this->longueur * $this->largeur;
     }
     }
-
+// ajout de class carre heritiere de la classe principale MaClasse
     class Carre extends MaClass{
 
         public $longueurCoté;
@@ -21,11 +22,18 @@
             $this->longueurCoté = $longueurCoté;
         }
 
-        public function calculSurface(){
-            return $longeurCoté * $longeurCoté;
+        // implementation de la methode calcul surface dans la class Carre
+        public function calculerSurface(){
+            return $this->longueurCoté * $this->longueurCoté;
         }
     }
 
+    // instance de la classe Carre et appel de le fonction calculSurface
+    $instanceCarre = new Carre(10);
+    $surfaceCarre = $instanceCarre->calculerSurface();
+    echo "la surface du carre est $surfaceCarre m²\n";
+
+ // ajout de class Cercle heritiere de la class principale MaClass
     class Cercle extends MaClass{
 
         public $pi;
@@ -36,9 +44,14 @@
             $this->rayon = $rayon;
         }
 
-        
-        public function calculSurface(){
-            return $pi * ($rayon * $rayon);
+        // implementation de la methode calcul surface dans la class cercle
+        public function calculerSurface(){
+            return $this->pi * ($this->rayon * $this->rayon);
         }
     }
+
+    // instance  de la classe Cercle et appel de le fonction calculSurface
+    $instanceCercle = new Cercle(3.14, 10);
+    $surfaceCercle = $instanceCercle->calculerSurface();
+    echo "la surface du cercle est $surfaceCercle m²";
 ?>
