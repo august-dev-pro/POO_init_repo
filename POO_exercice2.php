2bd3b9db6b5ea2bdbe268c9c7f00c17a1032002c
august-dev-pro <augustinselete01@gmail.com>
2023-09-17 01:06:28 +0000

    modification: ajout d'une instance et appel a la methode affichierDetail

--- POO_exercice2.php
+++ POO_exercice2.php
@@ -6,8 +6,13 @@
 
         // ajout de la methode AfficherDetails
         public function afficherDetails(){
-            echo " je suis $nom et j'ai $age ans";
-
+            echo "je suis $nom et j'ai $age ans";
         }
     }
+
+    // intance
+    $donnees = new personne();
+
+    //appel a la methode afficherDetails
+    $donnees -> afficherDetails();
 ?>
